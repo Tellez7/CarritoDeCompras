75a898a0d7ecd2ab1fe216d80df9ef03e14a7293
Esteban Tellez <es-tellez@hotmail.com>
2020-09-12 23:17:04 -0500

    Creacion Proyecto

---
+++ CarritoCompras/src/test/java/com/mainsoft/carritocompras/CarritoComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainsoft.carritocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
